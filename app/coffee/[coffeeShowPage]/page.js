a4c2d718fb56bbd9d7c80fffdb26774bbad4f36d
SquatingCyrus <thvsd.10fingers@gmail.com>
2023-09-20 14:55:49 -0400

    created routes to shop, and coffee show page

---
+++ app/coffee/[coffeeShowPage]/page.js
@@ -0,0 +1,7 @@
+export default function CoffeeShowPage() {
+  return (
+    <div>
+      <h1>Coffee Show Page</h1>
+    </div>
+  );
+}
---
+++ app/shop/page.js
@@ -0,0 +1,7 @@
+export default function Shop() {
+  return (
+    <div>
+      <h1>SHOPPING</h1>
+    </div>
+  );
+}
