e1bb1dc8559ba9bc4206dd59e7ce7e57c828da3c
SquatingCyrus <thvsd.10fingers@gmail.com>
2023-09-20 11:49:05 -0400

    created layout, navBar, footer

---
+++ app/components/NavBar.js
@@ -0,0 +1,21 @@
+import React from "react";
+import AppBar from "@mui/material/AppBar";
+import Button from "@mui/material/Button";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+
+const NavBar = () => {
+  return (
+    <AppBar position="sticky">
+      <Toolbar className="flex">
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          Logo/Name
+        </Typography>
+        <Button variant="contained">Log In</Button>
+        <Button variant="contained">Cart</Button>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default NavBar;
