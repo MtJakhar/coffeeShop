b5f513fc2733865fc363eb1bd985fe68eeeebb09
MtJakhar <thvsd.10fingers@gmail.com>
2023-10-19 15:02:39 -0400

    added url data to brands

--- app/coffee/[coffeeShowPage]/page.js
+++ app/coffee/[coffeeShowPage]/page.js
@@ -1,7 +1,9 @@
+import Image from "next/image";
 export default function CoffeeShowPage() {
   return (
     <div>
       <h1>Coffee Show Page</h1>
+      <Image src="https://lh3.googleusercontent.com/pw/ADCreHctYLwmMfJEw0g57UMSDa2OMlAerIdAWPH8jf1Amf1mK6fwvAMVrqihk7AVkisNiDOQk_MGOscwo0l-WFC7eu9t1h9WdUK-6TSB8Xydp2Dpzw_94WU=w2400" width={500} height={500} />
     </div>
   );
 }
