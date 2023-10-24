4b7fbf9b3c8479c0818c86e5cc949ca6b0c51860
MtJakhar <thvsd.10fingers@gmail.com>
2023-10-24 16:16:34 -0400

    removed image

--- app/coffee/[coffeeShowPage]/page.js
+++ app/coffee/[coffeeShowPage]/page.js
@@ -3,7 +3,6 @@ export default function CoffeeShowPage() {
   return (
     <div>
       <h1>Coffee Show Page</h1>
-      <Image src="https://lh3.googleusercontent.com/pw/ADCreHctYLwmMfJEw0g57UMSDa2OMlAerIdAWPH8jf1Amf1mK6fwvAMVrqihk7AVkisNiDOQk_MGOscwo0l-WFC7eu9t1h9WdUK-6TSB8Xydp2Dpzw_94WU=w2400" width={500} height={500} />
     </div>
   );
 }
