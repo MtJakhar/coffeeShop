b23f8ce7274c14fb9ff881cd4aa98751c4545989
MtJakhar <thvsd.10fingers@gmail.com>
2023-10-29 16:35:00 -0400

    created skeleton of coffee card

---
+++ app/components/CoffeeCard.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import Image from 'next/image'
+
+const CoffeeCard = () => {
+  return (
+    <div className="justify-center border-2 border-black p-4 text-center w-96">
+      <Image
+        className='mx-auto' 
+        src="https://lh3.googleusercontent.com/pw/ADCreHfbfgtwA0okW4ymmX1KmJNm8Qa7vj4oLDrmw983i1Bj2bH5pF2tnomHHxDu6G0YrSztVBsI2GJaKjaRNWrw8KPOmwBxeLs3IqEJmjGMpv2MnPt17jU=w1920-h1080"
+        width={500}
+        height={500}
+        alt="Coffee Image"
+      />
+      <hr/>
+      <h3>Brand name</h3>
+      <h1>Coffee Name</h1>
+      <p>$18.50</p>
+    </div>
+  )
+}
+
+export default CoffeeCard
--- app/shop/page.js
+++ app/shop/page.js
@@ -1,7 +1,10 @@
+import CoffeeCard from "../components/CoffeeCard";
+
 export default function Shop() {
   return (
     <div>
       <h1>SHOPPING</h1>
+      <CoffeeCard />
     </div>
   );
 }
