deb27a2890fbcb23d580a195ed39c0609ad35d19
MtJakhar <thvsd.10fingers@gmail.com>
2023-10-30 10:11:11 -0400

    refactor, reformat

--- app/components/CoffeeCard.js
+++ app/components/CoffeeCard.js
@@ -3,21 +3,21 @@ import Image from "next/image";
 
 const CoffeeCard = (props) => {
 	return (
-    <>
-      <div className="justify-center border-2 border-black text-center">
-        <Image
-          className="mx-auto"
-          src={props.coffee.image[0]}
-          width={500}
-          height={500}
-          alt="Coffee Image"
-        />
-        <hr />
-        <h3>{props.brandName.name}</h3>
-        <h1>{props.coffee.name}</h1>
-        <p>{props.coffee.price}</p>
-      </div>
-    </>
+		<>
+			<div className="justify-center border-2 border-black text-center">
+				<Image
+					className="mx-auto"
+					src={props.coffee.image[0]}
+					width={500}
+					height={500}
+					alt="Coffee Image"
+				/>
+				<hr />
+				<h3>{props.brandName.name}</h3>
+				<h1>{props.coffee.name}</h1>
+				<p>{props.coffee.price}</p>
+			</div>
+		</>
 	);
 };
 
--- app/shop/page.js
+++ app/shop/page.js
@@ -4,31 +4,37 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 const getCoffeeData = async () => {
-  const coffees = await prisma.coffee.findMany()
+	const coffees = await prisma.coffee.findMany();
 
-  return coffees
-}
+	return coffees;
+};
 
 const getBrandData = async () => {
-  const brands = await prisma.brand.findMany()
+	const brands = await prisma.brand.findMany();
 
-  return brands
-}
+	return brands;
+};
 export default async function Shop() {
-  const coffeeData = await getCoffeeData();
-  const brandData = await getBrandData();
-  console.log(brandData)
+	const coffeeData = await getCoffeeData();
+	const brandData = await getBrandData();
 
-  return (
-    <>
-      <div>
-        <h1>SHOPPING</h1>
-        <div className="grid md:grid-cols-4 grid-cols-1 gap-6 m-4">
-          {coffeeData.map((coffee) => {
-           return <CoffeeCard coffee={coffee} brandName={brandData.find((brand) => brand.id === coffee.brand_id)}/>
-          })}
-        </div>
-      </div>
-    </>
-  );
+	return (
+		<>
+			<div>
+				<h1>SHOPPING</h1>
+				<div className="grid md:grid-cols-4 grid-cols-1 gap-6 m-4">
+					{coffeeData.map((coffee) => {
+						return (
+							<CoffeeCard
+								coffee={coffee}
+								brandName={brandData.find(
+									(brand) => brand.id === coffee.brand_id
+								)}
+							/>
+						);
+					})}
+				</div>
+			</div>
+		</>
+	);
 }
