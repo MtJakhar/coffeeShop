ee4734d1e53d573c8cfc5e71b41547d5522b0d4d
MtJakhar <thvsd.10fingers@gmail.com>
2023-10-30 15:48:45 -0400

    refactored coffeeCard, added brands

--- app/components/CoffeeCard.js
+++ app/components/CoffeeCard.js
@@ -1,21 +1,21 @@
 import React from "react";
 import Image from "next/image";
 
-const CoffeeCard = (props) => {
+const CoffeeCard = ({ brandName, coffee }) => {
 	return (
 		<>
 			<div className="justify-center border-2 border-black text-center">
 				<Image
 					className="mx-auto"
-					src={props.coffee.image[0]}
+					src={coffee.image[0]}
 					width={500}
 					height={500}
 					alt="Coffee Image"
 				/>
 				<hr />
-				<h3>{props.brandName.name}</h3>
-				<h1>{props.coffee.name}</h1>
-				<p>{props.coffee.price}</p>
+				<h3>{brandName.name}</h3>
+				<h1>{coffee.name}</h1>
+				<p>{coffee.price}</p>
 			</div>
 		</>
 	);
