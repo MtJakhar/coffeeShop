9e819af6c12e80eb7627c63f90040ff39e7f69e3
MtJakhar <thvsd.10fingers@gmail.com>
2023-10-30 08:55:43 -0400

    fixed spacing between cols

--- app/shop/page.js
+++ app/shop/page.js
@@ -2,9 +2,21 @@ import CoffeeCard from "../components/CoffeeCard";
 
 export default function Shop() {
   return (
-    <div>
-      <h1>SHOPPING</h1>
-      <CoffeeCard />
-    </div>
+    <>
+      <div>
+        <h1>SHOPPING</h1>
+        <div className="grid grid-cols-4 gap-6 m-4">
+          <CoffeeCard />
+          <CoffeeCard />
+          <CoffeeCard />
+          <CoffeeCard />
+          <CoffeeCard />
+          <CoffeeCard />
+          <CoffeeCard />
+          <CoffeeCard />
+          <CoffeeCard />
+        </div>
+      </div>
+    </>
   );
 }
